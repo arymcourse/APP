8fef554e0041086538e19270e68d5472e4b6af68
IuliiaMB <iuliia.mitkevych@web.de>
2024-02-11 19:44:55 +0100

    ДЗ-2

---
+++ src/task_1.js
@@ -0,0 +1,15 @@
+function toggleFullscrenn() {
+    var element = document.getElementById('fullscreenElement');
+
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+        element.style.background = 'black';
+        element.style.color = 'white';
+    } else {
+        element.requestFullscreen().catch((err) => {
+            console.error('Error attempting to enable fullscreen:', err);
+    });
+    element.style.background = 'white';
+    element.style.color = 'black';            
+}
+}
